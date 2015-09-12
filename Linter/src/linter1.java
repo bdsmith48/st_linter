2bb722d62d54660873ace6a3f9768039764a37be
bdsmith48 <bailey.d.smith48@gmail.com>
2015-09-12 14:40:17 -0500

    creation of the Linter class, although it contains some error with regex that I am trying to fix.

---
+++ Linter/src/linter1.java
@@ -0,0 +1,64 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.regex.*;
+
+
+public class linter1 {
+	public static void main(String args[]){
+		Scanner console = new Scanner(System.in);
+		System.out.println("File: ");
+		String input = console.next();
+		File file = new File(input);
+		Scanner scan;
+		try {
+			scan = new Scanner(file);
+			String line = scan.nextLine();
+			int line_num = 1;
+			boolean okay = semicolon(line);
+			if(okay == false){
+				System.out.printf("<%d>. Should end with a semicolon", line_num);
+			}
+			while(scan.hasNextLine()){
+				line = scan.nextLine();
+				line_num++;
+				okay = semicolon(line);
+				if(okay == false){
+					System.out.printf("<%d>. Should end with a semicolon", line_num);
+				}
+			}
+			console.close();
+			scan.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("File not found");
+		}
+	}
+	public static boolean semicolon(String line){
+		boolean okay = false;
+		Matcher match = Pattern.compile(";$").matcher(line);
+		if(match.matches()){
+			okay = true;
+		}
+		else{
+			okay = bracket(line);
+		}
+		return okay;
+	}
+	public static boolean bracket(String line){
+		boolean okay = false;
+		if(line.length() == 1){
+			Matcher match = Pattern.compile("}$").matcher(line);
+			if(match.matches()){
+				okay = true;
+			}
+		}
+		else{
+			Matcher match = Pattern.compile("{$").matcher(line);
+			if(match.matches()){
+				okay = true;
+			}
+		}
+		return okay;
+	}
+
+}
